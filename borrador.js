e4cd0170e18670091d88427e878150b305dc79c0
mariachi9999 <nicolas.lupo.86@gmail.com>
2021-01-10 12:52:29 -0300

    Segundo commit, esta vez en carpeta principal

---
+++ borrador.js
@@ -0,0 +1,11 @@
+function sonIguales(x, y) {
+    // Devuelve "true" si "x" e "y" son iguales
+    // De lo contrario, devuelve "false"
+    // Tu código:
+    if (x = y) {
+      return true
+    } 
+    return false
+  }
+
+return sonIguales(15,15)
